30c6e1c12b32e8408457727039ee8da9aa7e5a1b
Hi-Sahil <sahilstudy5758@gmail.com>
2024-10-30 22:15:19 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict "; // treat all js code as newer version
+// alert(3 + 3); //we are using node js environment not browser
+
+let name  =" sahil"
+let age = 20 
+let isLoggedIn = false 
+let state ;
+
+//number => 2 to power 53
+// big int 
+//string =>""
+//Boolean =>true/false
+//null=> Standalone value
+// undefined =>
+// symbol=> unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
