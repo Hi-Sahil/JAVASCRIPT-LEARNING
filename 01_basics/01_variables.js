b01b0ea7e6f7cf307498d8f0076c167015ed8f57
Hi-Sahil <sahilstudy5758@gmail.com>
2024-10-30 22:02:15 +0000

    Learned about Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "sahil@github";
+var accountPassword = "12345";
+accountCity = "jaipur";
+let accountState ;
+//accountId = 2 ;
+accountEmail = "sahil2@youtube" ;
+accountPassword = "252550"
+accountCity = "Bangalore";
+ 
+console.log(accountId);
+/*
+Var is not preffered to declare variables becausae of issues in 
+block scope and variable Scope
+*/
+ console.table([accountEmail,accountId,accountPassword,accountCity , accountState]);
+ 
