819212904c5da462abc93f6996ac967d611e2cfb
Hi-Sahil <sahilstudy5758@gmail.com>
2024-11-05 19:20:56 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//let score = "33abc" ;
+//let score = null ;
+//let score = undefined ;
+//let score = true ;
+let score = "abc" ;
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+ let valueinNumber = Number(score);
+//  console.log(typeof valueinNumber);
+//  console.log(valueinNumber);
+
+ /*
+ 33=>33 ;
+ "33abc"=> NaN
+ true =>1 : false => 0 
+ */ 
+let isLoggedIn = 1 
+let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
+
+// 1 => true  ; 0=> false
+// "" => false 
+// "sahil"=> True 
+
+let someNumber= 33 ;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
