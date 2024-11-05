762360d79df8d16988288537ef46123b211ce1f6
Hi-Sahil <sahilstudy5758@gmail.com>
2024-11-05 19:40:50 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,40 @@ let booleanisLoggedIn = Boolean(isLoggedIn);
 
 let someNumber= 33 ;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ____________________OPERATIONS______________________
+
+let value = 3 ;
+let negValue = -value ;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "sahil"
+
+let str3 = str1 + str2 ;
+// console.log(str3);
+// console.log( "2" + 1);
+// console.log(2 +"1");
+// console.log( 1 + 2 +"1"); // toPrimitive
+// console.log("1" + 2 + 1);
+
+
+console.log(+true);
+console.log(+"");
+
+
+let num1 , num2 , num3;
+num1  = num2 = num3  = 2+2; // not good practice // readability of code is bad
+
+let a = 100 ;
+++a ;
+console.log(a);
