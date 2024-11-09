2a98d4d0e17dfee377ae8d97da611b88867de53c
Hi-Sahil <sahilstudy5758@gmail.com>
2024-11-09 18:38:26 +0000

    arrays 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,33 @@
+// array 
+const array =  [ 1 ,2 ,3  ,4 ,5 ]
+
+const myheroes = [ 'ironman ', 'luffy']
+const myarray = new Array(1,2,3,4,5)
+console.log(myarray[1]);
+
+// myarray.push(6);
+// console.log(myarray);
+
+// myarray.pop()
+
+
+//myarray.unshift(0);
+//myarray.shift();
+
+// console.log(myarray.includes(9));
+// console.log(myarray.indexOf(9));
+
+// const newArr = myarray.join()
+// console.log(myarray);
+// console.log( typeof newArr);
+
+
+console.log("A" , myarray);
+const myn1 = myarray.slice(1 , 3)
+console.log(myn1);
+console.log("B " , myarray);
+const myn2 = myarray.splice(1 , 3)
+console.log(myn2);
+console.log("C " , myarray);
+
+
