fae301fbfd42ee34370edee2e3281742974c9140
Hi-Sahil <sahilstudy5758@gmail.com>
2024-12-01 20:58:04 +0000

    function part2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,7 +22,7 @@ function Add2Numbers(num1 , num2){
     
     }
 const result = Add2Numbers(3,5);
-           //arguements
+                          //arguements
  // console.log("result :" , result);
 
  function LoginUserName(userName){
@@ -31,5 +31,30 @@ const result = Add2Numbers(3,5);
     }
    else {return`${userName} just logged in`}
  }
- console.log(LoginUserName())
+ //console.log(LoginUserName())
            
+
+function calculateCartPrice(val1 , val2 , ...num1){ //... rest operator
+          return num1
+}
+// console.log(calculateCartPrice(2 , 4 , 6 , 8));
+
+const user ={
+    username : "hitesh" , 
+    prices:199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);  
+}
+//handleObject(user)
+handleObject({
+    username:'sam ',
+    price : 399
+})
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,700,300,500]));
